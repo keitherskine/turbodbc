2d0819c6e16e2bafa4fa238dde1f2f150d2b94f8
Keith Erskine <toastie604@gmail.com>
2020-01-25 11:13:37 -0600

    tweaks

--- travis/check_mssql_is_available.sh
+++ travis/check_mssql_is_available.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Sometimes MSSQL is not immediately available, hence check this.
-# https://github.com/microsoft/mssql-docker/issues/203
+# Sometimes MSSQL is not immediately available when an MSSQL
+# docker instance has been started, hence check this.
+# See: https://github.com/microsoft/mssql-docker/issues/203
 
 container=$1
 server=$2
@@ -11,9 +12,10 @@ password=$4
 counter=1
 while true
 do
-  if docker exec -it "$container" /opt/mssql-tools/bin/sqlcmd -S "$server" -U "$user" -P "$password" -Q "SELECT @@VERSION"; then
+  if docker exec -it "$container" /opt/mssql-tools/bin/sqlcmd -S "$server" -U "$user" -P "$password" -Q "SELECT @@VERSION"
+  then
     echo "MSSQL available"
-    exit 0  # success
+    exit 0
   fi
 
   if [ $counter -ge 3 ]; then
